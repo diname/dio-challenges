1ad43d842c22a3be51d1b721c7d53e610aa639b6
Lucas Medina <lucasmedinarmc@gmail.com>
2023-12-09 12:16:11 -0300

    feat: Add a new condition for the players lives

--- detona-ralph/src/scripts/engine.js
+++ detona-ralph/src/scripts/engine.js
@@ -3,13 +3,15 @@ const state = {
     squares: document.querySelectorAll('.square'),
     enemy: document.querySelector('.enemy'),
     timeLeft: document.querySelector('#time-left'),
-    score: document.querySelector('#score')
+    score: document.querySelector('#score'),
+    lives: document.querySelector('#lives')
   },
   values: {
     gameVelocity: 1000,
     hitPosition: 0,
     result: 0,
-    currentTime: 60
+    currentTime: 60,
+    lives: 3
   },
   actions: {
     timerId: setInterval(randomSquare, 1000),
@@ -27,7 +29,7 @@ function countDown() {
   state.values.currentTime--
   state.view.timeLeft.textContent = state.values.currentTime
 
-  if (state.values.currentTime == 0) {
+  if (state.values.currentTime <= 0 || state.values.lives === 0) {
     playSound('gameover')
     clearInterval(state.actions.countDownTimer)
     clearInterval(state.actions.timerId)
@@ -54,6 +56,9 @@ function addListenerHitBox() {
         state.view.score.textContent = state.values.result
         state.values.hitPosition = null
         playSound('hit')
+      } else {
+        state.values.lives--
+        state.view.lives.textContent = state.values.lives
       }
     })
   })
